d9a666f419c4cbde81edf23f6209974c9db8a26c
Aurélien Pelletier <aurelien.pelletier@gmail.com>
2012-10-23 10:35:54 +0200

    Remplacement des system.out par un logger slf4j+logback

--- badCalculator/src/main/java/com/sfeir/badcalculator/Calculator.java
+++ badCalculator/src/main/java/com/sfeir/badcalculator/Calculator.java
@@ -1,15 +1,21 @@
 package com.sfeir.badcalculator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println(Calculator.compute("3 plus 4"));
-        System.out.println(Calculator.compute("3 moins 4"));
-        System.out.println(Calculator.compute("3 fois 4"));
-        System.out.println(Calculator.compute("3 divisé 4"));
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.compute("3 plus 4"));
+        System.out.println(calculator.compute("3 moins 4"));
+        System.out.println(calculator.compute("3 fois 4"));
+        System.out.println(calculator.compute("3 divisé 4"));
     }
 
-    private static Integer compute(String input) {
+    Logger log = LoggerFactory.getLogger(Calculator.class);
+
+    public Integer compute(String input) {
         int opsi = input.indexOf(' ');
         int opei = input.lastIndexOf(' ');
         String operation = input.substring(opsi + 1, opei);
@@ -19,17 +25,14 @@ public class Calculator {
         int leftValue = Integer.parseInt(input.substring(0, input.indexOf(' ')));
         int rightValue = Integer.parseInt(input.substring(input.lastIndexOf(' ') + 1));
 
+        log.info("Opération: {}", operation);
         if("plus".equals(operation)) {
-            System.out.println("Plus");
             return leftValue + rightValue;
         } else if ("moins".equals(operation)) {
-            System.out.println("Moins");
             return leftValue - rightValue;
         } else if ("fois".equals(operation)) {
-            System.out.println("Fois");
             return leftValue * rightValue;
         }  else if ("divisé".equals(operation)) {
-            System.out.println("Divisé");
             return leftValue / rightValue;
         }
 
