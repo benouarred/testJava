7dd662a593d3f00234419e0cab59f88ef433bdfc
Aurélien Pelletier <aurelien.pelletier@gmail.com>
2012-10-23 10:49:59 +0200

    Utilisation d'une expression régulière pour gérer des cas un peu plus complexe

--- badCalculator/src/main/java/com/sfeir/badcalculator/Calculator.java
+++ badCalculator/src/main/java/com/sfeir/badcalculator/Calculator.java
@@ -10,25 +10,25 @@ public class Calculator {
         System.out.println(calculator.compute("3 plus 4"));
         System.out.println(calculator.compute("3 moins 4"));
         System.out.println(calculator.compute("3 fois 4"));
-        System.out.println(calculator.compute("3 divisé 4"));
+        System.out.println(calculator.compute("3 divisÃ© 4"));
     }
 
     Logger log = LoggerFactory.getLogger(Calculator.class);
 
     public Integer compute(String input) {
-        String[] inputs = input.split(" ");
+        String[] inputs = input.split("\\s++");
         int leftValue = Integer.parseInt(inputs[0]);
         String operation = inputs[1];
         int rightValue = Integer.parseInt(inputs[2]);
 
-        log.info("Opération: {}", operation);
+        log.info("OpÃ©ration: {}", operation);
         if("plus".equals(operation)) {
             return leftValue + rightValue;
         } else if ("moins".equals(operation)) {
             return leftValue - rightValue;
         } else if ("fois".equals(operation)) {
             return leftValue * rightValue;
-        }  else if ("divisé".equals(operation)) {
+        }  else if ("divisÃ©".equals(operation)) {
             return leftValue / rightValue;
         }
 
