61fef099e4a4479171eedabd8a0411f8d4da1efb
bootis <bootis@gmail.com>
2013-03-01 15:05:55 +0100

    Added DotNet Version

---
+++ BadCalculatorDotNet/BadCalculator/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadCalculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.Compute("3 plus 4"));
+            Console.WriteLine(Calculator.Compute("3 moins 4"));
+            Console.WriteLine(Calculator.Compute("3 fois 4"));
+            Console.WriteLine(Calculator.Compute("3 divisé 4"));
+        }
+
+        private static int? Compute(String input)
+        {
+            int opsi = input.IndexOf(' ');
+            int opei = input.LastIndexOf(' ');
+            String operation = input.Substring(opsi + 1, opei);
+
+            //System.out.println("_" + operation + "_");
+
+            if (operation == "plus")
+            {
+                Console.WriteLine("Plus");
+                return Int32.Parse(input.Substring(0, input.IndexOf(' '))) + Int32.Parse(input.Substring(input.LastIndexOf(' ') + 1));
+            }
+            else if (operation == "moins")
+            {
+                Console.WriteLine("Moins");
+                return Int32.Parse(input.Substring(0, input.IndexOf(' '))) - Int32.Parse(input.Substring(input.LastIndexOf(' ') + 1));
+            }
+            else if (operation == "fois")
+            {
+                Console.WriteLine("Fois");
+                return Int32.Parse(input.Substring(0, input.IndexOf(' '))) * Int32.Parse(input.Substring(input.LastIndexOf(' ') + 1));
+            }
+            else if (operation == "divisé")
+            {
+                Console.WriteLine("Divisé");
+                return Int32.Parse(input.Substring(0, input.IndexOf(' '))) / Int32.Parse(input.Substring(input.LastIndexOf(' ') + 1));
+            }
+
+            return null;
+
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-# Tests pour le recrutement
+﻿# Tests pour le recrutement
 Ce repo contient des exercices pour évaluer les candidats derrière le clavier
 
 # Exercices
 
 ## JAVA
-- BadCalcultor: Exercice trivial pour vérifier les compétences de base en java
+- BadCalculator: Exercice trivial pour vérifier les compétences de base en java
 - PetClinic: Application Spring MVC/ Spring Core / JPA-Hibernate pour vérifier si le candidat est à l'aise avec ces frameworks
+
+## DOT NET
+- BadCalculator: Exercice trivial pour vérifier les compétences de base en .NET
