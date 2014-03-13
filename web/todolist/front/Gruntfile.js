8e4a2bd8ce8cd7dc01883a2ae0ead7b0c4a8a704
Thierry LAU <thierlau@gmail.com>
2014-03-13 18:48:19 +0100

    decorellation Front Server

---
+++ web/todolist/front/Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+	'use strict';
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+
+		connect : {
+			dev : {
+				options : {
+					hostname : 'localhost',
+					base: 'app',
+					port : 8888,
+					open : true,
+					keepalive: true,
+					middleware : function (connect, options) {
+						var proxy = require('grunt-connect-proxy/lib/utils').proxyRequest;
+						return [
+							proxy,
+							connect.static(options.base),
+							connect.directory(options.base)
+						];
+					}
+				},
+				proxies : [
+					{
+						context : '/api',
+						host : 'localhost',
+						port : 3000
+					}
+				]
+			}
+		}
+	});
+
+	grunt.registerTask('server', ['configureProxies:dev', 'connect:dev']);
+
+};
