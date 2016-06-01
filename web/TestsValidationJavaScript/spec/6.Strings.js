ddecf35726f5713ff6d6d4b4d044075f3c9f47c2
Siegfried Ehret <siegfried@ehret.me>
2016-06-01 22:22:16 +0200

    :gift: add test on strings
    Just to test string concatenation VS template strings

--- web/TestsValidationJavaScript/spec/6.Strings.js
+++ web/TestsValidationJavaScript/spec/6.Strings.js
@@ -1,31 +1,31 @@
-describe('Strings', function(){
+describe('Strings', function() {
 
-    it('Strings - 1', function(){
+    it('Strings - 1', function() {
         var singleQuotedString = 'apple';
         var doubleQuotedString = "apple";
         expect(singleQuotedString === doubleQuotedString).toBe( _ );
         expect(typeof(singleQuotedString)).toBe( _ );
         expect(typeof(doubleQuotedString)).toBe( _ );
-
     });
 
-    it('Strings - 2', function(){
+    it('Strings - 2', function() {
         var fruit = "apple";
         var dish = "pie";
-        expect( _ ).toBe( "apple pie." );
+        expect( _ ).toBe( "apple pie." ); // Use concatenation
+        expect( _ ).toBe( "apple pie." ); // Use template strings
     });
 
-    it('Strings - 3', function(){
+    it('Strings - 3', function() {
         var characterType = typeof("Amory".charAt(1));
         expect(characterType).toBe( _ );
     });
 
-    it('Strings - 4', function(){
+    it('Strings - 4', function() {
         var result = "hello";
         expect(result.length).toBe( _ );
     });
 
-    it('Strings - 5', function(){
+    it('Strings - 5', function() {
         var fruit = "pineapples";
         expect(fruit.slice( _, _)).toBe("apple");
     })
