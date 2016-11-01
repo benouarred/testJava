82e0ea82b5d1dabe1e08241bdb751d74939c5d18
Julian Sabos <artpick@gmail.com>
2016-11-01 10:37:15 +0100

    [JSA] New test Led Zeppelin
    Attempted features:
    - Inheritance,
    - Null pointers,
    - Static initialization
    - Pointer issues (override pointer references)

---
+++ java/ledzeppelin/src/main/java/com/sfeir/ledzeppelin/Album.java
@@ -0,0 +1,5 @@
+package com.sfeir.ledzeppelin;
+
+public class Album {
+
+}
---
+++ java/ledzeppelin/src/main/java/com/sfeir/ledzeppelin/Musician.java
@@ -0,0 +1,12 @@
+package com.sfeir.ledzeppelin;
+
+import java.util.Date;
+
+public class Musician {
+
+	private String name;
+	private String firstName;
+	private Date birthDay;
+private 
+	
+}
